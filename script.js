3e91a20ba540064553d8be46dc5eb1d4a18d91c5
IzzySoprano <Zerezghi@live.co.uk>
2021-06-17 21:30:51 +0000

    Added template to work from in flipping the cards

---
+++ script.js
@@ -0,0 +1,8 @@
+// Flipping cards
+
+const cards = document.querySelectorAll('.soprano-card');
+
+function flipCard () {
+    this.querySelectorAll.toggle(".soprano-card");
+}
+cards.forEach((soprano-game) =>); card.addEventListener("click", flipCard);
