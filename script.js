effa78e8d20d8cf6edfb45ea0e48ce01ed49a670
IzzySoprano <Zerezghi@live.co.uk>
2021-07-03 17:52:09 +0000

    Added shuffling function - not 100% complete

--- script.js
+++ script.js
@@ -10,179 +10,160 @@ cards.forEach((card) => card.addEventListener("click", flipCard));
 // Matching the cards
 
 let cardsArray = [
-  { name: "tony", img: "tony-soprano.jpg", },
-  
-  { name: "adriana-la-cerva", img: "adriana-la-cerva.jpg", },
+  { name: "tony", img: "Tony-Soprano.jpg" },
 
-  { name: "anthony-soprano-jr", img: "anthony-soprano-jr.jpg", },
+  { name: "adriana-la-cerva", img: "Adriana-La-Cerva.jpg" },
 
-  { name: "artie-bucco", img: "artie-bucco.jpg", },
+  { name: "anthony-soprano-jr", img: "Anthony-Soprano-Jr.jpg" },
 
-  { name: "bobby-baccalieri", img: "bobby-baccalieri.jpg", },
+  { name: "artie-bucco", img: "Artie-Bucco.jpg" },
 
-  { name: "camela-soprano", img: "camela-soprano.jpg", },
+  { name: "bobby-baccalieri", img: "Bobby-Baccalieri.jpg" },
 
-  { name: "carmine-lupertazzi", img: "carmine-lupertazzi.jpg", },
+  { name: "camela-soprano", img: "Camela-Soprano.jpg" },
 
-  { name: "christopher-moltisanti", img: "christopher-moltisanti.jpg", },
+  { name: "carmine-lupertazzi", img: "Carmine-Lupertazzi.jpg" },
 
-  { name: "corrado-soprano", img: "corrado-soprano.jpg", },
+  { name: "christopher-moltisanti", img: "Christopher-Moltisanti.jpg" },
 
-  { name: "dr-elliot-kupferberg", img: "dr-elliot-kupferberg.jpg", },
+  { name: "corrado-soprano", img: "Corrado-Soprano.jpg" },
 
-  { name: "dr-jennifer-melfi", img: "dr-jennifer-melfi.jpg", },
+  { name: "dr-elliot-kupferberg", img: "Dr-Elliot-Kupferberg.jpg" },
 
-  { name: "gloria-trillo", img: "gloria-trillo.jpg", },
-  
-  { name: "hesh-rabkin", img: "hesh-rabkin.jpg", },
-  
-  { name: "janice-soprano", img: "janice-soprano.jpg", },
-  
-  { name: "johnny-sacramoni", img: "johnny-sacramoni.jpg", },
-  
-  { name: "livia-soprano", img: "livia-soprano.jpg", },
-  
-  { name: "meadow-soprano", img: "meadow-soprano.jpg", },
-  
-  { name: "paulie-walnut", img: "paulie-walnut.jpg", },
-  
-  { name: "silvio-dante", img: "silvio-dante.jpg", },
+  { name: "dr-jennifer-melfi", img: "Dr-Jennifer-Melfi.jpg" },
 
-  { name: "sal-bonspensiero", img: "sal-bonspensiero.jpg", },
-  ];
+  { name: "gloria-trillo", img: "Gloria-Trillo.jpg" },
+
+  { name: "hesh-rabkin", img: "Hesh-Rabkin.jpg" },
+
+  { name: "janice-soprano", img: "Janice-Soprano.jpg" },
+
+  { name: "johnny-sacramoni", img: "Johnny-Sacramoni.jpg" },
+
+  { name: "livia-soprano", img: "Livia-Soprano.jpg" },
+
+  { name: "meadow-soprano", img: "Meadow-Soprano.jpg" },
+
+  { name: "paulie-walnut", img: "Paulie-Walnut.jpg" },
+
+  { name: "silvio-dante", img: "Silvio-Dante.jpg" },
+
+  { name: "sal-bonspensiero", img: "Sal-Bonpensiero.jpg" },
+];
 
 // Grab the div with a class of root
 
-const game = document.getElementsByClassName('Soprano-Game')
+const game = document.getElementsByClassName("Soprano-Game");
 
 // Create a section with a class of grid
 
-const grid = document.createElement('section')
-
-
+const grid = document.createElement("section");
 
 // Matching the list
 
-
-
-
-
-
-
-
-
 // Shuffling the cards
 
-let shuffleArray = Array => {
-  for (let i = array.length; i > 0; i--) {
+let shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
     const a = Math.floor(Math.random() * (i + 1));
-    const cardsArray = array[i];
-    array[i] = array[a];
-    array[cardsArray] = temp;
+    [array[i], array[a]] = [array[a], array[i]];
   }
-
-  return array;
+  return cardsArray;
 };
 
+function cardsToDom(array) {
+  const container = document.querySelector('.Soprano-game');
+  
+  for (let i of array) {
+    const code = `
+        <div class="Soprano-card">
+          <div class="Back-card">
+              <img class="Soprano_image" src="assets/img/Characters/${i.img}" alt="Characters">
+          </div>
+          <div class="front-card">
+              <img class="Soprano_image" src="assets/img/Sopranos-Title.jpg" alt="Sopranos-Title">
+          </div>
+      </div>
+    `;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Music Player
-const musicContainer = document.querySelector(".music-container")
-const playBtn = document.getElementById("play")
-const pauseBtn = document.getElementById("pause")
-const audio = document.getElementById("audio")
-const progress = document.querySelector(".music-container")
-const progressContainer = document.querySelector(".progress-container")
-const title = document.querySelector("#title")
-const cover = document.querySelector("#cover")
-const imgContainer = document.querySelector('.img-container')
-
-
-playBtn.addEventListener("click", () => {
-
-musicContainer.classList.add('music-container.play')
-playBtn.querySelector('i.fas').classList.add('fa-play')
-audio.play()
-})
-
-pauseBtn.addEventListener("click",() => {
-   musicContainer.classList.remove('play')
-   playBtn.querySelector('i-fas').classList.remove('fa-play')
-   
-   audio.pause()
+    container.innerHTML += code;
+  }
 }
-)
 
-// Event Listeners
+function init() {
+  let random = shuffleArray(cardsArray)
+  cardsToDom(random);
+}
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Music Player
+// const musicContainer = document.querySelector(".music-container")
+// const playBtn = document.getElementById("play")
+// const pauseBtn = document.getElementById("pause")
+// const audio = document.getElementById("audio")
+// const progress = document.querySelector(".music-container")
+// const progressContainer = document.querySelector(".progress-container")
+// const title = document.querySelector("#title")
+// const cover = document.querySelector("#cover")
+// const imgContainer = document.querySelector('.img-container')
 
 // playBtn.addEventListener("click", () => {
-//    const isPlaying = musicContainer.classList.contains('play');
-   
-//    if (isPlaying) {
-//       pauseSong();
-//    } else {
-//       playSong()
-//   }
-// });
 
-// Progress Bar
+// musicContainer.classList.add('music-container.play')
+// playBtn.querySelector('i.fas').classList.add('fa-play')
+// audio.play()
+// })
 
-function updateProgress(e) {
-const { duration, currentTime } = e.srcElement
-const progressPercent = (currentTime / duration) * 100
-progress.style.width = `${progressPercent}%`
-}
+// pauseBtn.addEventListener("click",() => {
+//    musicContainer.classList.remove('play')
+//    playBtn.querySelector('i-fas').classList.remove('fa-play')
 
-function setProgress(e) {
-  const width = this.clientwidth
-  const clickX = e.offsetX
-  const duration = audio.duration
+//    audio.pause()
+// }
+// )
 
-  audio.currentTime = (clickX / width) * duration
-}
+// // Event Listeners
 
- audio.addEventListener('timeupdate', updateProgress)
+// // playBtn.addEventListener("click", () => {
+// //    const isPlaying = musicContainer.classList.contains('play');
 
- progressContainer.addEventListener('click', setProgress)
+// //    if (isPlaying) {
+// //       pauseSong();
+// //    } else {
+// //       playSong()
+// //   }
+// // });
+
+// // Progress Bar
+
+// // function updateProgress(e) {
+// // const { duration, currentTime } = e.srcElement
+// // const progressPercent = (currentTime / duration) * 100
+// // progress.style.width = `${progressPercent}%`
+// // }
+
+// // function setProgress(e) {
+// //   const width = this.clientwidth
+// //   const clickX = e.offsetX
+// //   const duration = audio.duration
+
+// //   audio.currentTime = (clickX / width) * duration
+// // }
+
+// //  audio.addEventListener('timeupdate', updateProgress)
+
+// //  progressContainer.addEventListener('click', setProgress)
